e27d4be99d9d53978fe59cc0b585cddf46126e0c
David Sifry <dsifry@David-Sifrys-MacBook-Pro-17.local>
2010-04-20 14:50:52 -0700

    Removed words and speech_type from the app. No need for them anymore. Whee!

--- app/models/speech_type.rb
+++
@@ -1,3 +0,0 @@
-class SpeechType < ActiveRecord::Base
-  has_many :words
-end
--- app/models/word.rb
+++
@@ -1,7 +0,0 @@
-class Word < ActiveRecord::Base
-  belongs_to :speech_type
-
-  def speech_type=(val)
-    self.speech_type_id = val.to_i
-  end
-end
